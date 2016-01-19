64ebdebbaaf31b2cfbaeb0902c01f3ba7b4ad5ae
sheikirfan <irfan.sheik@imaginea.com>
2016-01-20 00:00:32 +0530

    adding changed files of kibana

---
+++ src/ui/public/stringify/__tests__/index.js
@@ -0,0 +1,9 @@
+describe('Stringify Component', function () {
+  require('./_conformance');
+  require('./_ip');
+  require('./_source');
+  require('./_string');
+  require('./_url');
+  require('./_color');
+  require('./_date');
+});
